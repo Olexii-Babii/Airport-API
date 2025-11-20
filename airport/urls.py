f2d23b62c08bf09c63c9d65f6b0faba13ff68821
Olexii <babiy0617@gmail.com>
2025-11-20 13:09:09 +0200

    Add airports endpoint
    -Create AirportSerializer in serializers.py
    -Create AirportViewSet in views.py
    -Create route in urls.py

--- airport/urls.py
+++ airport/urls.py
@@ -1,7 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
 
+from airport import views
+
+router = routers.DefaultRouter()
+router.register("airports", views.AirportViewSet)
 
 urlpatterns = [
-
+    path("", include(router.urls)),
 ]
 
 app_name = "airport"
