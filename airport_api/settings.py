5dce74c8232c061097134253533dae6144e8426b
Olexii <babiy0617@gmail.com>
2025-11-29 18:50:12 +0200

    Remove secret_key from settings.py

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e#uv-hx#92=1#1@ogq%mqsi)ojxik9s(flln$1h&_r0f8&s9zf"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
